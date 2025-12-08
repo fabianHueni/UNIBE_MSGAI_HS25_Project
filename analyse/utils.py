a0c66c7a81d925e80225ae9f520c3ad45fbe69bf
Fabian Hüni <fabian@huenis.ch>
2025-12-08 20:55:24 +0100

    feat: add analysis for report

--- analyse/utils.py
+++ analyse/utils.py
@@ -107,36 +107,158 @@ def plot_time_measures_overlaid(experiment_data, labels):
     plt.show()
 
 
-
-
-
-def plot_characters_vs_inference_time(experiment_data, labels):
+def plot_inference_time_distribution(experiment_data, labels, model_names):
     import matplotlib.pyplot as plt
     import seaborn as sns
+
     # Set the style for seaborn
     sns.set(style="whitegrid")
 
-    plt.figure(figsize=(7, 4))
+    fig, axes = plt.subplots(nrows=len(experiment_data), ncols=len(experiment_data[0]), figsize=(14, 10))
+    for j, (model_experiments, model_labels, model_name) in enumerate(zip(experiment_data, labels, model_names)):
+        colors = sns.color_palette("husl", len(experiment_data))  # Use seaborn color palette
 
-    # Loop through each DataFrame and plot
-    for df, label in zip(experiment_data, labels):
-        if 'number_of_characters' in df.columns and 'inference_time_ms' in df.columns:
-            plt.scatter(df['number_of_characters'], df['inference_time_ms'], alpha=0.5, label=label)
+        # take the maximum of all experiments for consistent x limit
+        max_x_limit = max([df['inference_time_ms'].quantile(0.98) for df in model_experiments])
 
-    plt.title('Inference Time vs Number of Input Characters', fontsize=12)
-    plt.xlabel('Number of Input Characters', fontsize=12)
-    plt.ylabel('Inference Time (ms)', fontsize=12)
-    plt.grid()
-    plt.legend(title='Experiments', fontsize=10)
+
+        # Loop through each DataFrame and plot
+        for i, (df, label) in enumerate(zip(model_experiments, model_labels)):
+            sns.histplot(df['inference_time_ms'].dropna(), bins=50, color=colors[j], label=f'{label}', alpha=0.6, kde=True, ax=axes[i][j])
+
+            if i == 0:
+                axes[i][j].set_title(model_name, fontsize=12)
+                axes[i][j].set_xlabel('', fontsize=12)
+            elif i == len(model_experiments) - 1:
+                axes[i][j].set_xlabel('Inference Time (ms)', fontsize=12)
+            else:
+                axes[i][j].set_title('', fontsize=12)
+                axes[i][j].set_xlabel('', fontsize=12)
+            if j == 0:
+                axes[i][j].set_ylabel('Frequency', fontsize=12)
+            else:
+                axes[i][j].set_ylabel('', fontsize=12)
+
+            axes[i][j].grid()
+            axes[i][j].legend(fontsize=10)
+
+            # Set consistent x and y limits for better comparison
+            axes[i][j].set_xlim(0, max_x_limit)
+            axes[i][j].set_ylim(0, None)
+
+    # remove not used subplots
+    fig.delaxes(axes[3][2])
+    fig.delaxes(axes[3][3])
+
+
+    fig.suptitle('Distribution of Inference Time over Model and Devices', fontsize=20)
     plt.tight_layout()
+
+    # save plot as png
+    plt.savefig('./plots/inference_time_distribution.png', dpi=300)
+
     plt.show()
 
+
+
+
+def plot_warm_up_phase(experiment_data, labels, model_names):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    # Set the style for seaborn
+    sns.set(style="whitegrid")
+
+    fig, axes = plt.subplots(nrows=len(experiment_data), ncols=len(experiment_data[0]), figsize=(14, 10))
+
+    for j, (model_experiments, model_labels, model_name) in enumerate(zip(experiment_data, labels, model_names)):
+        colors = sns.color_palette("husl", len(experiment_data))  # Use seaborn color palette
+
+
+        # Loop through each DataFrame and plot
+        for i, (df, label) in enumerate(zip(model_experiments, model_labels)):
+            df_sorted = df.sort_values('job_start_ts').reset_index(drop=True)
+            # calculate moving average over 3 samples
+            df_sorted['inference_time_ms'] = df_sorted['inference_time_ms'].rolling(window=5).mean()
+
+            first_20 = df_sorted.head(20)
+            axes[i][j].plot(first_20.index, first_20['inference_time_ms'], marker='o', linestyle='-', color=colors[i], label=f'{label}')
+            axes[i][j].set_title(f'Warmup Effect - {label}')
+            axes[i][j].set_xlabel('Sample Index')
+            axes[i][j].set_ylabel('Inference Time (ms)')
+            axes[i][j].grid(True, alpha=0.3)
+
+
+            if i == 0:
+                axes[i][j].set_title(model_name, fontsize=12)
+                axes[i][j].set_xlabel('', fontsize=12)
+            elif i == len(model_experiments) - 1:
+                axes[i][j].set_xlabel('Inference Time (ms)', fontsize=12)
+            else:
+                axes[i][j].set_title('', fontsize=12)
+                axes[i][j].set_xlabel('', fontsize=12)
+            if j == 0:
+                axes[i][j].set_ylabel('Frequency', fontsize=12)
+            else:
+                axes[i][j].set_ylabel('', fontsize=12)
+
+            axes[i][j].grid()
+            axes[i][j].legend(fontsize=10)
+
+
+    # remove not used subplots
+    fig.delaxes(axes[3][2])
+    fig.delaxes(axes[3][3])
+
+
+    fig.suptitle('Distribution of Inference Time over Model and Devices', fontsize=20)
+    plt.tight_layout()
+
+    # save plot as png
+    plt.savefig('./plots/inference_time_distribution.png', dpi=300)
+
+    plt.show()
+
+
+
+
+def plot_characters_vs_inference_time(experiment_data, labels, model_names):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    import numpy as np
+    # Set the style for seaborn
+    sns.set(style="whitegrid")
+
+    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(14, 10))
+    axes_flat = axes.flatten()
+    for ax, model_experiments, model_labels, model_name in zip(axes_flat, experiment_data, labels, model_names):
+
+        # Loop through each DataFrame and plot
+        for df, label in zip(model_experiments, model_labels):
+            if 'number_of_characters' in df.columns and 'inference_time_ms' in df.columns:
+                # generate a size vector to set all sizes to 1 for better visibility
+                size = np.ones(len(df)) * 5
+                ax.scatter(df['number_of_characters'], df['inference_time_ms'], alpha=0.6, label=label, sizes=size)
+
+        ax.set_title(model_name, fontsize=12)
+        ax.set_xlabel('Number of Input Characters', fontsize=12)
+        ax.set_ylabel('Inference Time (ms)', fontsize=12)
+        ax.grid()
+        ax.legend(title='Experiments', fontsize=10)
+
+    fig.suptitle('Inference Time vs. Input Characters', fontsize=20)
+    plt.tight_layout()
+
+    # save plot as png
+    plt.savefig('./plots/characters_vs_inference_time.png', dpi=300)
+
+    plt.show()
+
+def calc_correlation_characters_inference(experiment_data, labels):
     # Calculate and print correlation for each DataFrame
     for df, label in zip(experiment_data, labels):
         correlation = df['number_of_characters'].corr(df['inference_time_ms'])
         print(f'Correlation between number of characters and inference time for {label}: {correlation}')
-
-
 
 
 def fit_mm1_model(df):
